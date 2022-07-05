32b3f3ad4f7a3872be10fe6e295ab1b57f9afcfa
emre çakır <cakiremre17@hotmail.com>
2022-07-05 16:49:42 +0300

    5 Temmuz

---
+++ OOP1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitsInStock = 3;
+            product1.UnitPrice = 500;
+
+            ProductManager productmanager = new ProductManager();
+            productmanager.Add(product1);
+
+
+            
+        }
+    }
+}
