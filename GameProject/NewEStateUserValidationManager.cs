6401c19f7f55c742157d2878baa6ec1647bf5aed
emre çakır <cakiremre17@hotmail.com>
2022-07-07 11:11:01 +0300

    7 Temmuz

---
+++ GameProject/NewEStateUserValidationManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class NewEStateUserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            return true;
+        }
+    }
+
+    internal interface IUserValidationService
+    {
+        bool Validate(Gamer gamer);
+    }
+}
